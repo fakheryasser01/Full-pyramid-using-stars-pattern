7557fffdbcf194f683ec17cdaa62691dcac55c32
fakheryasser01 <38509021+fakheryasser01@users.noreply.github.com>
2018-12-06 10:58:56 +0200

    Add files via upload

---
+++ Assignment1.120.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main (void)
+{
+
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+
+	unsigned int number ;
+
+	printf("Please enter number of rows  \n");
+	scanf("%d" , &number);
+for (int rows =1 ; number >= rows ; rows++)
+{
+for (int span = number-1 ; span >= rows ; span-- )
+{
+printf(" ");
+}
+for (int star = 1 ; star <= (2*rows)-1 ; star++ )
+{
+printf("*");
+}
+printf("\n");
+
+}
+
+
+//
+//	 for (int x= 0 ; rows >= x ; x++ )
+//	 {
+//
+//		 		 for (int i= x ; rows>= i ; i++ )
+//		 			 {
+//		 				 printf(" ");
+//		 			 }
+//
+//		 for (int k =1  ; k <= (2*x)-1 ; k++ )
+//					 {
+//						printf("*");
+//					 }
+//		 printf("\n");
+//	 }
+
+
+return 0;
+}
+
+
+
